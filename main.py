7d96e698e64de3399d301f93198787de0b7b4f79
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-11-24 10:32:29 +0300

    modifiy menu function to handle errors exception for user input

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# Within each step code will be tested to prevent unexpected errors
-
-# function : renderUserInterFace takes no parameter, responsible for rendering menu options for the user
+# function -> renderUserInterFace : takes no parameter, responsible for rendering menu options for the user
 def renderUserInterfaceOptions():
     print("\n     1. Open Tab")
     print("     2. Close Tab")
@@ -13,15 +11,26 @@ def renderUserInterfaceOptions():
     print("     9. Exit\n") 
 
 
-# function : Menu takes no parameter, responsible for handling menu functionality
+# function -> Menu : takes no parameter, responsible for handling menu functionality
 def Menu():
-
     exit_program = False # exit_program will severs as a flag variable 
 
+    # continue looping while exit_program is false
     while not exit_program:
         renderUserInterfaceOptions()
-        options = int(input("choose one of these options ? "))
-
-        print(options)
+        # handling Exceptions
+        try:
+            options = int(input("choose one of these options ? "))
+        except ValueError:
+            print("\ninvalid input a number must be entered - Try Again")
 
 Menu()
+
+
+
+
+
+# handling exceptions wasn't explained however during code review sessions some class mates uses them to handle user inputs so I had to look out for the offical documentation to learn how to implemet it in python URL : https://docs.python.org/3/tutorial/errors.html
+
+
+# Within each step code will be tested to prevent unexpected errors
