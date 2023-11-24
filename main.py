6d99e0ac3fdcc297e3cce1f166b5ecfe1c20ce87
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-11-24 07:40:04 +0300

    refactor code by creating a user Interface rendering function

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 # Within each step code will be tested to prevent unexpected errors
 
-# First let's start by creating our Menu function which will take no parameter and output options, one of them will be chosen by the user
+# function : renderUserInterFace takes no parameter, responsible for rendering menu options for the user
+def renderUserInterfaceOptions():
+    print("\n     1. Open Tab")
+    print("     2. Close Tab")
+    print("     3. Switch Tab")
+    print("     4. Display All Tabs")
+    print("     5. Open Nested Tab")
+    print("     6. Sort All Tabs")
+    print("     7. Save Tabs")
+    print("     8. Import Tabs")
+    print("     9. Exit\n") 
 
+
+# function : Menu takes no parameter, responsible for handling menu functionality
 def Menu():
-    exit_program = False
+
+    exit_program = False # exit_program will severs as a flag variable 
 
     while not exit_program:
-
-        print("     1. Open Tab")
-        print("     2. Close Tab")
-        print("     3. Switch Tab")
-        print("     4. Display All Tabs")
-        print("     5. Open Nested Tab")
-        print("     6. Sort All Tabs")
-        print("     7. Save Tabs")
-        print("     8. Import Tabs")
-        print("     9. Exit")    
-        
+        renderUserInterfaceOptions()
         options = int(input("choose one of these options ? "))
 
         print(options)
